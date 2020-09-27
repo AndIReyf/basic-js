241c0ee90617b24e9faf52913b2f3039a47a8f7d
AndIReyf <andrey.prudius@hotmail.com>
2020-09-27 15:53:50 +0200

    feat: add task transform array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) throw new Error('Not an array')
+
+    const newArr = []
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== '--discard-next'
+            && arr[i] !== '--double-next'
+            && arr[i] !== '--double-prev'
+            && arr[i] !== '--discard-prev') newArr.push(arr[i])
+        if (arr[i - 1] !== undefined
+            && arr[i - 2] !== "--discard-next"
+            && arr[i] === '--discard-prev') newArr.pop()
+        if (arr[i - 1] !== undefined
+            && arr[i - 2] !== "--discard-next"
+            && arr[i] === '--double-prev') newArr.push(arr[i - 1])
+        if (arr[i + 1] !== undefined && arr[i] === '--double-next') newArr.push(arr[i + 1])
+        if (arr[i + 1] !== undefined && arr[i] === '--discard-next') i++
+    }
+    return newArr
+}
